ca3b7e8308d3b7be7e85ef25bd17d19fcddf224b
silveriris <103170468+silveriris@users.noreply.github.com>
2024-09-27 21:34:10 +0800

    手写memcpy.cpp

--- 手写memcpy.cpp
+++ 手写memcpy.cpp
@@ -1,33 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int lengthOfLongestSubstring(string s) 
+template<typename T>
+void mymemcpy(T* dst, T* src, T n)
 {
-	unordered_set<char> set;
-	int res = 1;
-	int r = -1;
-	for ( int i = 0; i <= s.length(); i++)
+	void* d = dst;
+	for (int i = 0; i < n; i++)
 	{
-		if (i!=0)
-		{
-			set.erase(s[i - 1]);
-		}
-		while (r+1< s.length() &&!set.count(s[r+1]))
-		{
-			set.insert(s[r + 1]);
-			r++;
-		}
-		res = max(res, r - i + 1);
+		*dst = *src;
+		dst++;
+		src++;
 	}
-	return res;
 }
-
 int main()
 {
-	string s = "abcabcbb";
-	auto res = lengthOfLongestSubstring(s);
-	cout << res;
+	int src[5] = { 1,2,3,4,5 };
+	int dst[5];
+	mymemcpy(dst, src,static_cast<int>(sizeof(src)/sizeof(*src)));
+	for (auto var : dst)
+		cout << var;
 	return 0;
 }
-
-
